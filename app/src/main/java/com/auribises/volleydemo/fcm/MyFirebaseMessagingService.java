0611a2412d55a7ea6bc603357797f04d37e3f867
Ishant Kumar <er.ishant@gmail.com>
2017-11-04 12:09:06 +0530

    Google Firebase Messaging

---
+++ app/src/main/java/com/auribises/volleydemo/fcm/MyFirebaseMessagingService.java
@@ -0,0 +1,88 @@
+package com.auribises.volleydemo.fcm;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.auribises.englishterminal.R;
+import com.auribises.englishterminal.activities.HomeActivity;
+import com.auribises.englishterminal.helper.Utility;
+import com.auribises.volleydemo.R;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Eshaan on 05-Dec-16.
+ */
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    // [START receive_message]
+    Intent intent;
+
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        if (remoteMessage == null){
+            return;
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+
+            handleNotification(remoteMessage.getNotification().getBody());
+        }
+        if (remoteMessage.getData().size() > 0) {
+            Log.i( "Data Payload: " ,remoteMessage.getData().toString());
+            Log.i("size", String.valueOf(remoteMessage.getData().size()));
+
+            try {
+                JSONObject json = new JSONObject(remoteMessage.getData());
+                Log.i("TRY","inTry");
+                handleDataMessage(json);
+            } catch (Exception e) {
+                Log.i( "Exception: " , e.getMessage());
+            }
+        }
+    }
+
+    private void handleDataMessage(JSONObject json) {
+        Log.i( "push json: " , json.toString());
+
+
+    }
+
+    private void handleNotification(String message) {
+
+
+
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle("Notification")
+                    .setContentText(message);
+
+
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+
+
+
+
+    }
+}
